64e5d3cbaccc37f63f5d5543e2bfb7c77e32bb9d
Hariprasad <hari20185@gmail.com>
2023-04-12 08:37:39 +0530

    rabbit-mq-changes

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["self"]
+  owners      = ["973714476881"]
 }
 
 data "aws_route53_zone" "domain" {
