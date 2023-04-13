adcb024fa59d6d6624100106ff6291a144eb8ffe
Hariprasad <hari20185@gmail.com>
2023-04-13 10:50:08 +0530

    rabbit-mq-changes

--- data.tf
+++ data.tf
@@ -1,11 +1,10 @@
-/*
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "devops-practice-with-ansible"
   owners      = ["self"]
 }
 
-data "aws_route53_zone" "domain" {
+/*data "aws_route53_zone" "domain" {
   name = var.dns_domain
 }*/
 
